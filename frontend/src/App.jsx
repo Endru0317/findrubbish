68ad03632c622878cc375d7703017082d47f4aaf
aylin <aylin.mrLarki@gmail.com>
2022-05-13 11:10:36 +0200

    create map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import AddRubbish from "@pages/AddRubbish.jsx";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Map from "./pages/Map";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <AddRubbish />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="container">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="map" element={<Map />}></Route>
+            <Route path="addlocation" element={<AddRubbish />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
